3be4ff0f6ce78e571abf024a597d910d19bdfa3e
egmarie <gibson.lizzi@gmail.com>
2022-12-04 10:56:29 -0500

    fix db submission error in register.js

--- service/register.js
+++ service/register.js
@@ -1,16 +1,17 @@
 //var AWS  = require("aws-sdk");
-const { DynamoDB, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDB, PutItemCommand, GetItemCommand, ListTablesCommand } = require("@aws-sdk/client-dynamodb");
 const util = require('../utils/util');
 const bcrypt = require('bcryptjs');
 const dynamodb = new DynamoDB({
     region: 'us-east-1'
 });
+let saveUserResponse;
 const userTable = 'auth-practice-users'; // defined in Dynamo db
-
 async function register(userInfo) {
 
     const name = userInfo.name;
     const username = userInfo.username;
+    const usernameSearch = {"S": username.toLowerCase().trim()}
     const email = userInfo.email;
     const password = userInfo.password;
 
@@ -19,61 +20,80 @@ async function register(userInfo) {
             message: 'All fields are required'
         });
     }
-
-    // const dynamoUser = await getUser(username);
-    // if (dynamoUser && dynamoUser.username) {
-    //     return util.buildResponse(401, {
-    //         message: 'username already exists in our database. Please choose a different username.'
+    // let x = true
+    //     const tables = await getTable();
+    // if (x) {
+    //     return util.buildResponse(200, {
+    //         message: tables.TableNames.join("\n")
     //     });
-        
+    // } else {
+    //     console.log("huh")
     // }
+
+    const dynamoUser = await getUser(usernameSearch);
+    if (dynamoUser.Item && dynamoUser.Item.username) {
+        return util.buildResponse(401, {
+            message: `username already exists in our database. Please choose a different username. ${dynamoUser}`
+        });
+    }
 
     const encryptedPW = bcrypt.hashSync(password.trim(), 10);
 
     const user = {
-        name: name,
-        email: email,
-        username: username.toLowerCase().trim(),
-        password: encryptedPW
+        name: {"S": name},
+        email: {"S": email},
+        username: {"S": username.toLowerCase().trim()},
+        password: {"S": encryptedPW},
     };
 
-    const saveUserResponse = saveUser(user);
-        if (!saveUserResponse) {
-            
-            return util.buildResponse(503, {message: user});
+  try {
+    saveUserResponse = await saveUser(user);
+
+    if (saveUserResponse) {
+    return util.buildResponse(200, { body: `mission accomplished: ${saveUserResponse}` } );
+    }
+  } catch (err) {
+    if (!saveUserResponse) {
         
-        }
-    return util.buildResponse(200, { username: username }) ;
-}
+    return util.buildResponse(503, {message: "input not processed."});
+    } else {
+        console.log(err.stack);
+    }
+    }
+    }
     
-async function getUser(username) {
+  
+    
+
+
+ async function getUser(username) {
     const params = {
         TableName: userTable,
-         Item: {
-            primaryKey: username, 
+        Key: {
+            username: username, 
         },
     };
-    return await dynamodb.getItem(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else console.log(data);           // successful response
- });
+    const getInput = new GetItemCommand(params);
+  try {
+    let data = await dynamodb.send(getInput);
+    return data //JSON.stringify(data);
+  } catch (err) {
+    return err;
+  }
 }
 
 const saveUser = async (user) => {
     const params = {
         TableName: userTable,
-        Key: {
-            Item: user
-        }
+        Item: user
     };
+    const putInput = new PutItemCommand(params);
+
   try {
-    const data = await dynamodb.send(new PutItemCommand(params));
-    console.log(data);
-    return data;
+     let data = await dynamodb.send(putInput);
+     return JSON.stringify(data)
   } catch (err) {
-    console.error(err);
+    return err;
   }
 };
-
-
 module.exports.register = register;
